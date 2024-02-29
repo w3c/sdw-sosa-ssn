47e95f79cecb0733165d64089e0179e636c14311
Simon Cox <dr.shorthair@pm.me>
2024-02-29 17:57:09 +1100

    Combine Introduction and Origins Factor out Modularization

--- ssn/config.js
+++ ssn/config.js
@@ -26,7 +26,8 @@ var respecConfig = {
       company: "Universität Wien",
       companyURL: "https://www.univie.ac.at/",
       orcid: "0009-0003-1968-887X"
-    },
+    }],
+  formerEditors: [
     {
       name: "Armin Haller",
       company: "Australian National University",
@@ -47,15 +48,38 @@ var respecConfig = {
       key: "Contributors (ordered alphabetically)",
       data: [
         {
-          value: "Rob Atkinson, Metalinkage"
-        }, {
-          value: "Luis de Sousa, ISRIC"
-        }, {
-          value: "Sylvan Grellet, BRGM"
-        }, {
-          value: "Kathi Schleidt, Datacove"
-        }, {
-          value: "Hylke van der Schaaf, Fraunhofer IOSB"
+          name: "Rob Atkinson",
+          company: "Metalinkage",
+          orcid: "0000-0002-7815-2472"
+        },
+        {
+          name: "Luis de Sousa",
+          company: "ISRIC",
+          companyURL: "https://www.isric.org/",
+          orcid: "0000-0002-6668-7138"
+        },
+        {
+          name: "Sylvain Grellet",
+          company: "BRGM",
+          companyURL: "https://brgm.fr/",
+          orcid: "0000-0001-7656-1830"
+        },
+        {
+          name: "Kathi Schleidt",
+          company: "Datacove",
+          companyURL: "https://www.datacove.eu/",
+          orcid: "0000-0002-8011-7350"
+        },
+        {
+          name: "Hylke van der Schaaf",
+          company: "Fraunhofer IOSB",
+          companyURL: "https://www.iosb.fraunhofer.de",
+          orcid: "0000-0001-6110-8902"
+        },
+        {
+          name: "Rob Warren",
+          company: "Glengarry Agriculture and Farming",
+          companyURL: "https://github.com/GlengarryAg"
         }]
     },
     {
