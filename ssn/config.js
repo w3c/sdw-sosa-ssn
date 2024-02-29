b690b512eda3f1a58624469c2e21214cf355d34c
Simon Cox <dr.shorthair@pm.me>
2024-02-29 22:28:35 +1100

    xrefs

--- ssn/config.js
+++ ssn/config.js
@@ -48,38 +48,17 @@ var respecConfig = {
       key: "Contributors (ordered alphabetically)",
       data: [
         {
-          name: "Rob Atkinson",
-          company: "Metalinkage",
-          orcid: "0000-0002-7815-2472"
-        },
-        {
-          name: "Luis de Sousa",
-          company: "ISRIC",
-          companyURL: "https://www.isric.org/",
-          orcid: "0000-0002-6668-7138"
-        },
-        {
-          name: "Sylvain Grellet",
-          company: "BRGM",
-          companyURL: "https://brgm.fr/",
-          orcid: "0000-0001-7656-1830"
-        },
-        {
-          name: "Kathi Schleidt",
-          company: "Datacove",
-          companyURL: "https://www.datacove.eu/",
-          orcid: "0000-0002-8011-7350"
-        },
-        {
-          name: "Hylke van der Schaaf",
-          company: "Fraunhofer IOSB",
-          companyURL: "https://www.iosb.fraunhofer.de",
-          orcid: "0000-0001-6110-8902"
-        },
-        {
-          name: "Rob Warren",
-          company: "Glengarry Agriculture and Farming",
-          companyURL: "https://github.com/GlengarryAg"
+          value: "Rob Atkinson, Metalinkage"
+        }, {
+          value: "Luis de Sousa, ISRIC"
+        }, {
+          value: "Sylvain Grellet, BRGM"
+        }, {
+          value: "Kathi Schleidt, Datacove"
+        }, {
+          value: "Hylke van der Schaaf, Fraunhofer IOSB"
+        }, {
+          value: "Rob Warren, Glengarry Agriculture and Farming"
         }]
     },
     {
