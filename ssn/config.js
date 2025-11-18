6e6aa1fdb18c225030a869d6ffd2caac09a3e621
Simon Cox <dr.shorthair@pm.me>
2025-11-18 17:03:02 +1100

    Documentation of BFO alignment

--- ssn/config.js
+++ ssn/config.js
@@ -155,6 +155,18 @@ var respecConfig = {
     }
   ],
   localBiblio: {
+    "BFO": {
+        title: "Information technology — Top-level ontologies (TLO) — Part 2: Basic Formal Ontology (BFO)",
+        href: "https://www.iso.org/standard/74572.html",
+        rawDate: "2021-11",
+        publisher: "International Organization for Standardization (ISO)",
+        status: "ISO/IEC 21838-2:2021"
+    },
+    "CCO": {
+        title: "The Common Core Ontologies (CCO)",
+        href: "https://github.com/CommonCoreOntology/CommonCoreOntologies",
+        rawDate: "2022",
+    },
     "Description-Logics": {
       href: "http://www.cambridge.org/9780521150118",
       authors: ["Franz Baader", "Diego Calvanese", "Deborah L. McGuinness", "Daniele Nardi", "Peter F. Patel-Schneider"],
