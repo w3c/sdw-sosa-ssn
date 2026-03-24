e39133bf327e68633232930fd4a23f20c1274156
Simon Cox <dr.shorthair@pm.me>
2026-03-24 22:04:54 +1100

    copyright

--- ssn/usage/config.js
+++ ssn/usage/config.js
@@ -1,9 +1,9 @@
 var respecConfig = {
-  specStatus: "base",
+  specStatus: "NOTE",
   shortName: "ssn-2023-usage",
   //publishDate:  "2015-05-18",
   previousPublishDate: "2017-10-19",
-  previousMaturity: "Note",
+  previousMaturity: "NOTE",
   previousURI: "https://w3c.github.io/sdw/ssn-usage/",
   edDraftURI: "https://w3c.github.io/sdw-sosa-ssn/ssn-usage/",
   // lcEnd: "3000-01-01",
@@ -20,7 +20,6 @@ var respecConfig = {
   noIDLIn: true,
   noLegacyStyle: false,
   noRecTrack: false,
-  overrideCopyright: "<p class='copyright'><a href='https://www.w3.org/Consortium/Legal/ipr-notice#Copyright'>Copyright</a> © 2024 <a href='http://www.opengeospatial.org/'>OGC</a> &amp; <a href='https://www.w3.org/'> <abbr title='World Wide Web Consortium'>W3C</abbr> </a><sup>®</sup> (<a href='https://www.csail.mit.edu/'><abbr title='Massachusetts Institute of Technology'>MIT</abbr></a>, <a href='https://www.ercim.eu/'><abbr title='European Research Consortium for Informatics and Mathematics'>ERCIM</abbr></a>, <a href='https://www.keio.ac.jp/'>Keio</a>, <a href='https://ev.buaa.edu.cn/'>Beihang</a>), <abbr title='World Wide Web Consortium'>W3C</abbr> <a href='https://www.w3.org/Consortium/Legal/ipr-notice#Legal_Disclaimer'>liability</a>, <a href='https://www.w3.org/Consortium/Legal/ipr-notice#W3C_Trademarks'>trademark</a>, <a href='https://www.w3.org/Consortium/Legal/copyright-documents'>W3C</a> and <a href='https://www.ogc.org/ogc/document'>OGC</a> document use rules apply.</p>",
   logos: [
     {
       src: "images/OGC-0.png",
