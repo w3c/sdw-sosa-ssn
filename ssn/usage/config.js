bd0d36a9ddd3c1f8fe94e4ffffffb0dc7bb55610
Simon Cox <dr.shorthair@pm.me>
2026-03-24 21:41:36 +1100

    FIx up config.js

--- ssn/usage/config.js
+++ ssn/usage/config.js
@@ -2,27 +2,72 @@ var respecConfig = {
   specStatus: "base",
   shortName: "ssn-usage",
   //publishDate:  "2015-05-18",
-  //previousPublishDate: "2014-03-27",
-  //previousMaturity: "FPWD",
-  //previousURI: "http://www.w3.org/TR/2014/WD-tabular-data-model-20140327/",
+  previousPublishDate: "2017-10-19",
+  previousMaturity: "Document",
+  previousURI: "https://w3c.github.io/sdw/ssn-usage/",
   edDraftURI: "https://w3c.github.io/sdw-sosa-ssn/ssn-usage/",
   // lcEnd: "3000-01-01",
   // crEnd: "3000-01-01",
   editors: [
     {
-      name: "Simon Cox",
-      company: "OGC",
-      companyURL: "https://ogc.org/"
+      name: "Simon J D Cox",
+      w3cid: "1796",
+      company: "Timely Logic, AU",
+      orcid: "0000-0002-3884-3420",
+      w3cid: 1796
+    },
+    {
+      name: "Maxime Lefrançois",
+      w3cid: "50604",
+      company: "École Nationale Supérieure des Mines de Saint-Étienne, FR",
+      companyURL: "https://www.mines-stetienne.fr/",
+      orcid: "0000-0001-9814-8991"
+    },
+    {
+      name: "Rob Warren",
+      w3cid: "144476",
+      company: "Glengarry Agriculture and Forestry, CA",
+      companyURL: "https://github.com/GlengarryAg"
+    },
+    {
+      name: "Rob Atkinson",
+      w3cid: "90763",
+      company: "OGC & Metalinkage, AU",
+      companyURL: "https://www.ogc.org/",
+      orcid: "0000-0002-7815-2472"
+    },
+    {
+      name: "Luis Moreira de Sousa",
+      w3cid: "145885",
+      company: "Instituto Superior Técnico Lisboa, PT",
+      companyURL: "https://tecnico.ulisboa.pt/en/",
+      orcid: "0000-0002-5851-2071"
+    },
+    {
+      name: "Kathi Schleidt",
+      w3cid: "140963",
+      company: "Datacove, AT",
+      companyURL: "https://www.datacove.eu/",
+      orcid: "0000-0002-8011-7350"
+    },
+    {
+      name: "Sylvain Grellet",
+      w3cid: "143334",
+      company: "BRGM, FR",
+      companyURL: "https://brgm.fr/",
+      orcid: "0000-0001-7656-1830"
+    },
+    {
+      name: "Krzysztof Janowicz",
+      company: "Universität Wien, AT",
+      companyURL: "https://www.univie.ac.at/",
+      orcid: "0009-0003-1968-887X"
     },
     {
       name: "Nicholas Car",
       company: "KurrawongAI",
-      companyURL: "https://kurrawong.ai"
-    },
-    {
-      name: "Maxime Lefrançois",
-      company: "École Nationale Supérieure des Mines de Saint-Étienne",
-      companyURL: "http://www.mines-stetienne.fr/"
+      companyURL: "https://kurrawong.ai",
+      orcid: "0000-0002-8742-7730"
     }
   ],
   wg: "Spatial Data on the Web Working Group",
